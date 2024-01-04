f4a324d4132c00e332bcfe508bcb60ab9e3f86f4
Marian Kmotorka <marian.kmotorka@notino.com>
2024-01-04 15:15:53 +0100

    port

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.get("/stream-json", (req, res) => {
   streamJsonArrayWithTimeout(0);
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
